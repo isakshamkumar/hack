dd1d6f193c391d50d8fc3a7469d20c775df4947b
saksham <saksham@genflowly.com>
2024-01-28 08:34:17 +0530

    added signup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from "../src/components/Navbar/Index";
 import { createTheme, ThemeProvider } from "@mui/material";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
 function App() {
   const THEME = createTheme({
     typography: {
@@ -17,8 +19,13 @@ function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={THEME}>
-        <Navbar />
-        <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+        {/* <Navbar /> */}
+        <Routes>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/signin" element={<Signin/>}/>
+
+          {/* <Route path="/" element={<Home />} /> */}
+          </Routes>
       </ThemeProvider>
     </BrowserRouter>
   );
